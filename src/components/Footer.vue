0042285c8fdf5df0f5b9e58f8633792088c5afa1
Mohamadreza Golbaba <r.golbaba@extratik.com>
2023-04-13 19:35:39 +0300

    create footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <p>&copy; 2023 My Shop</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style>
+footer {
+  background-color: #eee;
+  color: #333;
+  padding: 1rem;
+  text-align: center;
+}
+</style>
